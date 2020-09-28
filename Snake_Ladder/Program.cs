128bfc2beebe18fa5acc4eb50ab6911641eb144b
Srushti <Srushti@DESKTOP-QP0QMA4>
2020-09-28 12:47:52 +0530

    [Srushti]Add.No rolled on die

--- Snake_Ladder/Program.cs
+++ Snake_Ladder/Program.cs
@@ -10,6 +10,11 @@ namespace Snake_Ladder
             int FINAL_POSITION = 100;
             
             int player1Position = 0;
+            Console.WriteLine("Player1 starting position :" + player1Position);
+
+            Random random = new Random();
+            int dieNo = random.Next(1, 7);
+            Console.WriteLine("No on die :" + dieNo);
         }
     }
 }
