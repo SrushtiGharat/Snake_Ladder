93707f2b6069c2e9cdb4f8ee9c5de98ee9ad7fa0
Srushti <Srushti@DESKTOP-QP0QMA4>
2020-09-28 23:33:36 +0530

    [Srushti]Add.Player2 Added

--- Snake_Ladder/Program.cs
+++ Snake_Ladder/Program.cs
@@ -2,54 +2,88 @@
 
 namespace Snake_Ladder
 {
+    
     class Program
     {
+        
+        public const int FINAL_POSITION = 100;
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Snake and Ladder game");
-            int FINAL_POSITION = 100;
 
             int countDieRoll = 0;
+            int chance = 0; //chance = 0, player1 turn     chance = 1,player2turn           
             int player1Position = 0;
+            int player2Position = 0;
+            Console.WriteLine("Player1 starting position :" + player1Position);
             Console.WriteLine("Player1 starting position :" + player1Position);
 
             Random random = new Random();
-            while(player1Position < FINAL_POSITION)
+            
+            while (player1Position < FINAL_POSITION && player2Position < FINAL_POSITION)
             {
                 int dieNo = random.Next(1, 7);
                 countDieRoll++;
-                Console.WriteLine("No on die :" + dieNo);
 
+               
                 int checkOption = random.Next(0, 3);
-                switch (checkOption)
+
+                if (chance == 0)
                 {
-                    case 0:
-                        Console.WriteLine("No Play");
-                        break;
-                    case 1:
-                        Console.WriteLine("Ladder");
-                        player1Position = player1Position + dieNo;
-                        if(player1Position > FINAL_POSITION)
-                        {
-                            player1Position = player1Position - dieNo;
-                        }
-                        
-                        break;
-                    case 2:
-                        Console.WriteLine("Snake");
-                        player1Position = player1Position - dieNo;
-                        if(player1Position < 0)
-                        {
-                            player1Position = 0;
-                        }
-                        break;
-                    default:
-                        Console.WriteLine("No such option");
-                        break;
+                    Console.WriteLine("Player1 Plays :");
+                    Console.WriteLine("No on die :" + dieNo);
+                    player1Position = GetPlayerPosition(player1Position,dieNo,checkOption);
+                    Console.WriteLine("Player1 position :"+player1Position);
+                    if(checkOption == 0 || checkOption == 2)
+                      chance = 1;     
                 }
-                Console.WriteLine("Your current position is :" + player1Position);
+                else
+                {
+                    Console.WriteLine("Player2 Plays :");
+                    Console.WriteLine("No on die :" + dieNo);
+                    player2Position = GetPlayerPosition(player2Position,dieNo,checkOption);
+                    Console.WriteLine("Player2 position :" + player2Position);
+                    if (checkOption == 0 || checkOption == 2)
+                        chance = 0;
+                }
             }
-            Console.WriteLine("Die Roll Count :" + countDieRoll);
+            Console.WriteLine("Total Die Roll :" + countDieRoll);
+            if(player1Position == 10)
+                Console.WriteLine("Player 1 WINS!!");
+            else
+                Console.WriteLine("Player 1 WINS!!");
+        }
+
+        static int GetPlayerPosition(int playerPosition,int dieNo,int checkOption)
+        {
+            switch (checkOption)
+            {
+                case 0:
+                    Console.WriteLine("No Play");
+                    break;
+                case 1:
+                    Console.WriteLine("Ladder");
+                    playerPosition = playerPosition + dieNo;
+                    if (playerPosition > FINAL_POSITION)
+                    {
+                        playerPosition = playerPosition - dieNo;
+                    }
+
+                    break;
+                case 2:
+                    Console.WriteLine("Snake");
+                    playerPosition = playerPosition - dieNo;
+                    if (playerPosition < 0)
+                    {
+                        playerPosition = 0;
+                    }
+                    break;
+                default:
+                    Console.WriteLine("No such option");
+                    break;
+            }
+            return playerPosition;
+
         }
     }
 }
